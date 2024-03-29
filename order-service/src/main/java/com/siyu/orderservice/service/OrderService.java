b09a2f120479bfff7979c97157cafadfa19d981a
Siyu Liu <siyu.java.web@gmail.com>
2024-03-29 17:52:41 +1100

    edits inventory service config to run on random port and allows multiple instances, edits webclient bean to start with load balancer, edits url for the http request from order service to inventory service by using the url registed in the Eureka discovery server

--- order-service/src/main/java/com/siyu/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/siyu/orderservice/service/OrderService.java
@@ -20,7 +20,7 @@ public class OrderService {
     private OrderRepository orderRepository;
 
     @Autowired
-    private WebClient webClient;
+    private WebClient.Builder webClientBuilder;
     
     public void placeOrder(OrderRequest orderRequest){
         Order order=new Order();
@@ -32,8 +32,8 @@ public class OrderService {
         order.setOrderLineItemList(orderLineItems);
         List<String> skuCodes=order.getOrderLineItemList().stream().map(OrderLineItems::getSkuCode).toList();
         //call inventory service and place order if product is in
-        InventoryResponse[] inventoryResponses= webClient.get()
-                .uri("http://localhost:8082/api/inventory",
+        InventoryResponse[] inventoryResponses= webClientBuilder.build().get()
+                .uri("http://inventory-service/api/inventory",
                         uriBuilder -> uriBuilder.queryParam("skuCode",skuCodes).build())
                 .retrieve()
                 .bodyToMono(InventoryResponse[].class)
