cae705b9cf3ab40cd0ff56161a48d162f45ee0e5
Siyu Liu <siyu.java.web@gmail.com>
2024-03-30 22:34:47 +1100

    fixes bugs

--- order-service/src/main/java/com/siyu/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/siyu/orderservice/controller/OrderController.java
@@ -2,6 +2,7 @@ package com.siyu.orderservice.controller;
 
 import com.siyu.orderservice.dto.OrderRequest;
 import com.siyu.orderservice.service.OrderService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -14,8 +15,14 @@ public class OrderController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+//    @CircuitBreaker(name = "inventory", fallbackMethod = "fallbackMethod")
     public String placeOrder(@RequestBody OrderRequest orderRequest){
         orderService.placeOrder(orderRequest);
         return "Order Place Successfully";
     }
+
+    public String fallbackMethod(OrderRequest orderRequest,RuntimeException runtimeException){
+        return "Oops! Something went wrong, please order after some time!";
+    }
+
 }
