7a2efc817283a53adcfdedf60a67ad4fc1f129ed
Siyu Liu <siyu.java.web@gmail.com>
2024-03-31 14:28:33 +1100

    adds distributed tracing mechanism using micrometer and zipkin

--- order-service/src/main/java/com/siyu/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/siyu/orderservice/service/OrderService.java
@@ -1,11 +1,14 @@
 package com.siyu.orderservice.service;
 
+import com.netflix.discovery.converters.Auto;
 import com.siyu.orderservice.dto.InventoryResponse;
 import com.siyu.orderservice.dto.OrderLineItemsDto;
 import com.siyu.orderservice.dto.OrderRequest;
 import com.siyu.orderservice.model.Order;
 import com.siyu.orderservice.model.OrderLineItems;
 import com.siyu.orderservice.repository.OrderRepository;
+import io.micrometer.observation.Observation;
+import io.micrometer.observation.ObservationRegistry;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -21,6 +24,8 @@ public class OrderService {
 
     @Autowired
     private WebClient.Builder webClientBuilder;
+    @Autowired
+    private ObservationRegistry observationRegistry;
     
     public String placeOrder(OrderRequest orderRequest){
         Order order=new Order();
@@ -31,22 +36,28 @@ public class OrderService {
                 .toList();
         order.setOrderLineItemList(orderLineItems);
         List<String> skuCodes=order.getOrderLineItemList().stream().map(OrderLineItems::getSkuCode).toList();
-        //call inventory service and place order if product is in
-        InventoryResponse[] inventoryResponses= webClientBuilder.build().get()
-                .uri("http://inventory-service/api/inventory",
-                        uriBuilder -> uriBuilder.queryParam("skuCode",skuCodes).build())
-                .retrieve()
-                .bodyToMono(InventoryResponse[].class)
-                .block();
-        boolean allProductsInStock= Arrays.stream(inventoryResponses).allMatch(InventoryResponse::isInStock);
 
-        if(allProductsInStock){
-            orderRepository.save(order);
-            return "Order Placed Successfully";
-        }else {
-            throw new IllegalArgumentException("Product is not in stock");
-        }
+        Observation inventoryServiceObservation = Observation.createNotStarted("inventory-service-lookup",
+                this.observationRegistry);
+        inventoryServiceObservation.lowCardinalityKeyValue("call", "inventory-service");
 
+        return inventoryServiceObservation.observe(()->{
+            //call inventory service and place order if product is in
+            InventoryResponse[] inventoryResponses= webClientBuilder.build().get()
+                    .uri("http://inventory-service/api/inventory",
+                            uriBuilder -> uriBuilder.queryParam("skuCode",skuCodes).build())
+                    .retrieve()
+                    .bodyToMono(InventoryResponse[].class)
+                    .block();
+            boolean allProductsInStock= Arrays.stream(inventoryResponses).allMatch(InventoryResponse::isInStock);
+
+            if(allProductsInStock){
+                orderRepository.save(order);
+                return "Order Placed Successfully";
+            }else {
+                throw new IllegalArgumentException("Product is not in stock");
+            }
+        });
     }
 
     private OrderLineItems mapToDto(OrderLineItemsDto orderLineItemsDto) {
