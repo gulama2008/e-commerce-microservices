0d4980ea0f2f13b691c5c98cc04423a6f9f25b7d
Siyu Liu <siyu.java.web@gmail.com>
2024-03-31 13:22:04 +1100

    adds timeout and retry mechanisms using r4j

--- inventory-service/src/main/java/com/siyu/inventoryservice/service/InventoryService.java
+++ inventory-service/src/main/java/com/siyu/inventoryservice/service/InventoryService.java
@@ -2,6 +2,8 @@ package com.siyu.inventoryservice.service;
 
 import com.siyu.inventoryservice.dto.InventoryResponse;
 import com.siyu.inventoryservice.repository.InventoryRepository;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -9,12 +11,13 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 
 @Service
-
+@Slf4j
 public class InventoryService {
     @Autowired
     private InventoryRepository inventoryRepository;
 
     @Transactional(readOnly = true)
+    @SneakyThrows
     public List<InventoryResponse> isInStock(List<String> skuCode){
         return inventoryRepository.findBySkuCodeIn(skuCode).stream().map(inventory->
             InventoryResponse.builder()
--- order-service/src/main/java/com/siyu/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/siyu/orderservice/controller/OrderController.java
@@ -3,9 +3,13 @@ package com.siyu.orderservice.controller;
 import com.siyu.orderservice.dto.OrderRequest;
 import com.siyu.orderservice.service.OrderService;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.concurrent.CompletableFuture;
 
 @RestController
 @RequestMapping("/api/order")
@@ -16,13 +20,14 @@ public class OrderController {
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     @CircuitBreaker(name = "inventory", fallbackMethod = "fallbackMethod")
-    public String placeOrder(@RequestBody OrderRequest orderRequest){
-        orderService.placeOrder(orderRequest);
-        return "Order Place Successfully";
+    @TimeLimiter(name = "inventory")
+    @Retry(name = "inventory")
+    public CompletableFuture<String> placeOrder(@RequestBody OrderRequest orderRequest){
+       return CompletableFuture.supplyAsync(()->orderService.placeOrder(orderRequest));
     }
 
-    public String fallbackMethod(OrderRequest orderRequest,RuntimeException runtimeException){
-        return "Oops! Something went wrong, please order after some time!";
+    public CompletableFuture<String> fallbackMethod(OrderRequest orderRequest,RuntimeException runtimeException){
+        return CompletableFuture.supplyAsync(()->"Oops! Something went wrong, please order after some time!");
     }
 
 }
